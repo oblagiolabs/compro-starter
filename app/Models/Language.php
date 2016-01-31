379ff2a3c760ed6aeedc63429b8e749aec3e0312
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-02-01 00:47:47 +0700

    make migration and model language

---
+++ app/Models/Language.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    public $guarded = [];
+
+    public function rules($id = "")
+    {
+    	return [
+
+    		'language' => 'required|max:30',
+
+    	];
+    }
+}
